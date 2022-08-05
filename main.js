c29fe59c14b5e6b7c0dadbc9577ecd507a0b1e46
alejox <you@example.comalejoxdrums@gmail.com>
2022-08-05 01:51:55 -0500

    post

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
-//Api
+/*--------------------API-------------------------*/
 
-const url = 'https://picsum.photos/v2/list?page=2&limit=3'
+const url = 'https://picsum.photos/v2/list?page=2&limit=4'
 
 fetch(url)
   .then(res => res.json())
-  .then(data => {
-
-    let element = document.getElementById('element')
-    element.innerHTML = `<img src='${data[0].download_url}' alt='Texto'/>`
-
-    console.log(data);
-  })
+  .then(data => viewData(data))
   .catch(err => console.log(err));
 
+const viewData = (data) => {
+  let body = '';
+  data.forEach(element => {
+    body +=
+      `<div class="swiper-slide">
+    <img src='${element.download_url}'/>
+        </div>`
+  });
+
+  document.getElementById('element').innerHTML = body
+}
 
 
- // Slider 
+/*------------------------Slider----------------------*/
 
 const swiper = new Swiper('.swiper', {
+
 
   // If we need pagination
   pagination: {
@@ -35,3 +41,39 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-scrollbar',
   },
 });
+
+/* ---------------Like Button ---------------- */
+
+const likeBtn = () => {
+  let heart = document.querySelector('.heart');
+  let likes = document.querySelector('.like');
+
+
+  if (heart.src.match('/assets/img/heart-blank.svg')) {
+    heart.src = "/assets/img/heart.svg";
+    likes.innerHTML = '3,501 likes';
+
+  } else {
+    heart.src = "/assets/img/heart-blank.svg";
+    likes.innerHTML = '3,500 likes';
+  }
+}
+
+document.querySelector('.heart').addEventListener("click", function () {
+  this.style.transform = "scale(1.2)";
+  setTimeout(() => {
+    this.style.transform = "scale(1)";
+  }, 500)
+});
+
+/*----------------------------Post------------------------*/
+
+
+function write(e) {
+  e.preventDefault()
+  const input = document.getElementById('create-post');
+  const value = input.value;
+  const resultP = document.getElementById('comment');
+  resultP.innerText = `${value}`
+}
+
